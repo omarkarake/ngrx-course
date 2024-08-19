f4651a3cfbab0a8f2b5d4822f8f7e05fdb4e4ece
omar karake <omar.karake@amalitechtraining.org>
2024-08-19 18:18:04 +0200

    Reading Data From The Store

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,28 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 
-import { CounterService } from '../counter.service';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
-
-  constructor(private counterService: CounterService) {}
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
-  }
-
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
+export class CounterOutputComponent {
+  count$: Observable<number>;
+  constructor(private store: Store<{counter: number}>) {
+    this.count$ = store.select('counter');
   }
 }
