3354df97c40e8114441ef5299ab465f9819d555b
omar karake <omar.karake@amalitechtraining.org>
2024-08-20 12:30:08 +0200

    installing ngrx effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { counterReducer } from './store/counter.reducer';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { counterReducer } from './store/counter.reducer';
   ],
   imports: [BrowserModule, StoreModule.forRoot({
     counter: counterReducer,
-  })],
+  }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
