74df1819fbec05ec16cc3df73f9767c6361de851
omar karake <omar.karake@amalitechtraining.org>
2024-08-20 10:13:42 +0200

    Introducing Actions & State Changing Reducers and dispatching actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,8 +1,13 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.actions';
 const initialState = 0;
-// export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, state => state - 1)
+);
 
 // other alternative way of creating reducer function
-export function counterReducer(state = initialState) {
-  return state;
-}
+// export function counterReducer(state = initialState) {
+//   return state;
+// }
